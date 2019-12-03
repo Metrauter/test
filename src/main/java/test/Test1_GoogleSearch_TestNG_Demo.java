d1db024a0f7ae2da976ca674d9d7a3bd6f49e16a
Serhii <serg.ua92@mail.ru>
2019-12-03 18:35:06 +0200

    first TestNG test

---
+++ src/main/java/test/Test1_GoogleSearch_TestNG_Demo.java
@@ -0,0 +1,42 @@
+package test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Test1_GoogleSearch_TestNG_Demo {
+
+    WebDriver driver = null;
+
+    @BeforeTest
+    public void setUpTest() {
+
+        String projectLocation = System.getProperty("user.dir");
+        System.setProperty("webdriver.chrome.driver", projectLocation + "\\lib\\chromedriver\\chromedriver.exe");
+        driver = new ChromeDriver();
+
+    }
+
+    @Test
+    public void googleSearch() {
+
+        driver.get("https://google.com");
+        driver.findElement(By.name("q")).sendKeys("Automation");
+        //driver.findElement(By.name("btnI")).click();
+        driver.findElement(By.name("q")).sendKeys(Keys.RETURN);
+
+    }
+
+    @AfterTest
+    public void tearDownTest() {
+
+        driver.close();
+        driver.quit();
+        System.out.println("Test completed");
+
+    }
+}
